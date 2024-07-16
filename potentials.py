5d6242e9c53ff13d100cc48db3abc91c0f76d15a
Bretton Simpson <87884207+brettonsimpson@users.noreply.github.com>
2024-07-16 10:28:47 -0500

    Imported NumPy and resolved variable typos

--- potentials.py
+++ potentials.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 def plummer_sphere_potential(radius, z, b, amplitude):
     return -amplitude/np.sqrt(radius**2 + z**2 + b**2)
 
 def NFW_potential(radius, a, amplitude):
     first_term = amplitude/4*np.pi*a**3
-    second_term = ((r/a)*(1+r/a)**2)**-1
+    second_term = ((radius/a)*(1+radius/a)**2)**-1
     return first_term*second_term
