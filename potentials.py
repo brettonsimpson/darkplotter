4fb596e7fd44cd23eb206539f386ef82c614da22
Bretton Simpson <87884207+brettonsimpson@users.noreply.github.com>
2024-07-16 10:24:03 -0500

    Added Plummer and NFW potentials

--- potentials.py
+++ potentials.py
@@ -1,2 +1,7 @@
-#test
-#test 2
+def plummer_sphere_potential(radius, z, b, amplitude):
+    return -amplitude/np.sqrt(radius**2 + z**2 + b**2)
+
+def NFW_potential(radius, a, amplitude):
+    first_term = amplitude/4*np.pi*a**3
+    second_term = ((r/a)*(1+r/a)**2)**-1
+    return first_term*second_term
