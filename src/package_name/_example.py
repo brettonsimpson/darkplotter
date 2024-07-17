4753fd07e3a667fe9f9d98b67112d9dfdd0df315
Keneth Garcia <102555105+KenethGarcia@users.noreply.github.com>
2024-07-16 19:31:55 -0500

    Update _example.py

--- src/package_name/_example.py
+++ src/package_name/_example.py
@@ -1,1 +1,3 @@
 # This Python file is an example of how to add the main code and its attributes to our package. The notation of using src previous to the main package_name directory is standard on Python packages
+
+# NOTE: Edit the name of the 'package_name' folder before adding code
