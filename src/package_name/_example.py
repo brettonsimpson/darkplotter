1d5784bb11520fe73d82672e6e05c841e1a0c19a
Keneth Garcia <102555105+KenethGarcia@users.noreply.github.com>
2024-07-16 19:30:46 -0500

    Create main code directory

---
+++ src/package_name/_example.py
@@ -0,0 +1,1 @@
+# This Python file is an example of how to add the main code and its attributes to our package. The notation of using src previous to the main package_name directory is standard on Python packages
