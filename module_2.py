8641589ed4eaaaf5a5ff9e5d975ed65a43e19d51
Bretton Simpson <astronautbrett@gmail.com>
2024-07-18 14:22:31 -0500

    Cleaning up the develop branch for bifurcated scripts and notebooks to be introduced

--- module_2.py
+++
@@ -1,4 +0,0 @@
-import plotly
-import scipy
-import numpy
-import matplotlib.pyplot as plt
--- potentials.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def plummer_sphere_potential(radius, z, b, amplitude):
-    return -amplitude/np.sqrt(radius**2 + z**2 + b**2)
-
-def NFW_potential(radius, a, amplitude):
-    first_term = amplitude/4*np.pi*a**3
-    second_term = ((radius/a)*(1+radius/a)**2)**-1
-    return first_term*second_term
