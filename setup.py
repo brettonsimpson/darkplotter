e7d0be86b3ae0f845edffd42192cb361ac9179f5
rathorevedant99 <rathorevedant99@gmail.com>
2024-07-22 14:18:42 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='darkplotter',
+    version='1.0.1',
+    description='A plotting library for dark-themed visualizations',
+    author='Test User'
+    url='https://github.com/rathorevedant99/darkplotter',
+    packages=['darkplotter'],
+    install_requires=[
+    "matplotlib",
+    "numpy",
+    "pandas",
+    "scipy",
+    "plotly"
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    )
