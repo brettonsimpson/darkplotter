59163828215e0f8c56fd3e3d52f7cdda83134c70
rathorevedant99 <rathorevedant99@gmail.com>
2024-07-22 16:00:43 +0530

    Added data files

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     author='Test User',
     url='https://github.com/rathorevedant99/darkplotter',
     packages=['darkplotter'],
+    package_data={'': ['MW_Vc.txt','MWay.dat']},
+    include_package_data=True,
     install_requires=[
     "matplotlib",
     "numpy",
